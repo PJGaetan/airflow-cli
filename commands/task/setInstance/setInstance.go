b6f3a7de3d16dbeaff64af834cfa9945bfdc3f94
PJGaetan <pjgaetan@hotmail.fr>
2023-06-25 18:09:20 +0200

    feat: set task & dag state from dag status cmd

--- commands/task/setInstance/setInstance.go
+++ commands/task/setInstance/setInstance.go
@@ -1,20 +1,12 @@
 package setInstance
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"os"
 	"reflect"
-	"sort"
-	"time"
 
-	"github.com/AlecAivazis/survey/v2"
-	"github.com/pjgaetan/airflow-cli/api/request"
 	"github.com/pjgaetan/airflow-cli/internal/constant"
 	"github.com/pjgaetan/airflow-cli/pkg/model"
 	"github.com/pjgaetan/airflow-cli/pkg/prompt"
-	"github.com/pjgaetan/airflow-cli/pkg/utils"
 
 	"github.com/spf13/cobra"
 )
@@ -62,91 +54,5 @@ func state(cmd *cobra.Command, args []string) {
 		}
 		DagRunId = run.DagRunId
 	}
-
-	responseTask := request.AirflowGetRequest("dags/" + DagId + "/dagRuns/" + DagRunId + "/taskInstances")
-	var taskInstance model.TaskInstances
-	if err := json.Unmarshal([]byte(responseTask), &taskInstance); err != nil {
-		panic(err)
-	}
-
-	instances := taskInstance.TaskInstance
-	// reverse sort to have last run in first
-	sort.Slice(instances, func(i, j int) bool {
-		return instances[i].StartDate.Format(time.RFC3339) > instances[j].StartDate.Format(time.RFC3339)
-	})
-	taskInstanceIds := make([]string, len(instances))
-	for index, task := range instances {
-		taskInstanceIds[index] = task.TaskId
-	}
-	if len(instances) == 0 {
-		fmt.Println("No task in this dag for this dag run.")
-	}
-	fmt.Println(DagRunId)
-
-	// Task selection
-	var taskId string
-	promptTask := &survey.Select{
-		Message: "What task do you want to set?",
-		Options: taskInstanceIds,
-		Description: func(value string, index int) string {
-			for _, i := range instances {
-				if i.TaskId == value {
-					if i.State == "" {
-						return "none"
-					}
-					return i.State
-				}
-			}
-			return ""
-		},
-	}
-	err := survey.AskOne(promptTask, &taskId)
-	utils.ExitIfError(err)
-	if taskId == "" {
-		return
-	}
-
-	// State selection
-	var state string
-	PromtpState := &survey.Select{
-		Message: "What state do you want to state?",
-		Options: []string{"success", "failed"},
-	}
-	err = survey.AskOne(PromtpState, &state)
-	utils.ExitIfError(err)
-
-	// Params selections
-	params := []string{}
-
-	promptParams := &survey.MultiSelect{
-		Message: "Who does it needs to apply to?",
-		Options: []string{"include_upstream", "include_downstream"},
-	}
-	err = survey.AskOne(promptParams, &params)
-	utils.ExitIfError(err)
-	mapParams := make(map[string]any)
-	// Default params
-	mapParams["include_future"] = false
-	mapParams["include_past"] = false
-	mapParams["include_downstream"] = false
-	mapParams["include_upstream"] = false
-	mapParams["dag_run_id"] = DagRunId
-
-	for _, p := range params {
-		mapParams[p] = true
-	}
-	mapParams["new_state"] = state
-	mapParams["task_id"] = taskId
-	mapParams["dry_run"] = false
-	jsonParams, err := json.Marshal(mapParams)
-	if err != nil {
-		log.Fatal("Error ", err)
-	}
-
-	response := request.AirflowPostRequest("dags/"+DagId+"/updateTaskInstancesState", string(jsonParams))
-	var dagRun model.DagRuns
-	if err := json.Unmarshal([]byte(response), &dagRun); err != nil {
-		panic(err)
-	}
-	fmt.Println(string(response))
+	prompt.PromptSetTaskInstanceState(DagId, DagRunId)
 }
